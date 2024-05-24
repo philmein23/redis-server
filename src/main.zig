aa33b88a2376d0d11b0191a04009e9d162c15cad
Phillip Nguyen <phillipnguyen@Phillips-MacBook-Air.local>
2024-05-25 03:24:42 +0700

    add tcp server

--- src/main.zig
+++ src/main.zig
@@ -3,24 +3,51 @@ const std = @import("std");
 const net = std.net;
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    try stdout.print("Logs from your program will appear here!", .{});
-
-    // Uncomment this block to pass the first stage
+    // const stdout = std.io.getStdOut().writer(); var gpa = std.heap.GeneralPurposeAllocator(.{}){}; defer gpa.deinit(); const allocator = gpa.allocator();
     //
-    const address = try net.Address.resolveIp("127.0.0.1", 6379);
+    // // You can use print statements as follows for debugging, they'll be visible when running tests.
+    // try stdout.print("Logs from your program will appear here!", .{});
+    //
+    // // Uncomment this block to pass the first stage
+    // //
+    // const address = try net.Address.resolveIp("127.0.0.1", 6379);
+    //
+    // var server = try address.listen(.{
+    //     .reuse_address = true,
+    // });
+    // defer server.deinit();
+    //
+    // // while (true) {
+    // const connection = try server.accept();
+    //
+    // try stdout.print("Connection received {} is sending data", .{connection.address});
+    // connection.stream.close();
+    //
+    // const message = connection.stream.reader().readAllAlloc(allocator, 1024);
+    // stdout.print("{} says {s}\n", .{ connection.address, message });
+    // // }
 
-    var listener = try address.listen(.{
-        .reuse_address = true,
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const loopback = try net.Ip4Address.parse("127.0.0.1", 6379);
+    const localhost = net.Address{ .in = loopback };
+    var server = try localhost.listen(.{
+        .reuse_port = true,
     });
-    defer listener.deinit();
+    defer server.deinit();
 
-    while (true) {
-        const connection = try listener.accept();
+    const addr = server.listen_address;
+    std.print("Listening on {}, access this port to end the program\n", .{addr.getPort()});
 
-        try stdout.print("accepted new connection", .{});
-        connection.stream.close();
-    }
+    var client = try server.accept();
+    defer client.stream.close();
+
+    std.print("Connection received! {} is sending data.\n", .{client.address});
+
+    const message = try client.stream.reader().readAllAlloc(allocator, 1024);
+    defer allocator.free(message);
+
+    std.print("{} says {s}\n", .{ client.address, message });
 }
