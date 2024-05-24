9352119e07050151703fd7ab9f0caf9b3f6ae886
Phillip Nguyen <phillipnguyen@Phillips-MacBook-Air.local>
2024-05-25 03:28:23 +0700

    test tcp

--- src/main.zig
+++ src/main.zig
@@ -3,14 +3,15 @@ const std = @import("std");
 const net = std.net;
 
 pub fn main() !void {
-    // const stdout = std.io.getStdOut().writer(); var gpa = std.heap.GeneralPurposeAllocator(.{}){}; defer gpa.deinit(); const allocator = gpa.allocator();
-    //
+    const stdout = std.io.getStdOut().writer();
+    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    // defer gpa.deinit();
+    // const allocator = gpa.allocator();
     // // You can use print statements as follows for debugging, they'll be visible when running tests.
     // try stdout.print("Logs from your program will appear here!", .{});
     //
     // // Uncomment this block to pass the first stage
-    // //
-    // const address = try net.Address.resolveIp("127.0.0.1", 6379);
+    // // const address = try net.Address.resolveIp("127.0.0.1", 6379);
     //
     // var server = try address.listen(.{
     //     .reuse_address = true,
@@ -39,15 +40,15 @@ pub fn main() !void {
     defer server.deinit();
 
     const addr = server.listen_address;
-    std.print("Listening on {}, access this port to end the program\n", .{addr.getPort()});
+    stdout.print("Listening on {}, access this port to end the program\n", .{addr.getPort()});
 
     var client = try server.accept();
     defer client.stream.close();
 
-    std.print("Connection received! {} is sending data.\n", .{client.address});
+    stdout.print("Connection received! {} is sending data.\n", .{client.address});
 
     const message = try client.stream.reader().readAllAlloc(allocator, 1024);
     defer allocator.free(message);
 
-    std.print("{} says {s}\n", .{ client.address, message });
+    stdout.print("{} says {s}\n", .{ client.address, message });
 }
