9c4b9008e48e33003522113ad38b651e1b5213a8
Phillip Nguyen <phillipnguyen@Phillips-MacBook-Air.local>
2024-05-26 10:06:36 +0700

    test

--- src/main.zig
+++ src/main.zig
@@ -7,64 +7,60 @@ pub fn main() !void {
     // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     // defer _ = gpa.deinit();
     // const allocator = gpa.allocator();
-    // // You can use print statements as follows for debugging, they'll be visible when running tests.
-    // try stdout.print("Logs from your program will appear here!", .{});
-    //
-    // // Uncomment this block to pass the first stage
-    // const address = try net.Address.resolveIp("127.0.0.1", 6379);
-    //
-    // var server = try address.listen(.{
-    //     .reuse_address = true,
-    // });
-    // defer server.deinit();
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    try stdout.print("Logs from your program will appear here!", .{});
 
-    // while (true) {
-    //     var client = try server.accept();
-    //
-    //     try stdout.print("Connection received {} is sending data", .{client.address});
-    //     client.stream.close();
-    //
-    //     const expected = "PONG";
-    //     const message = try client.stream.reader().readAllAlloc(allocator, expected.len);
-    //     defer allocator.free(message);
-    //
-    //     try stdout.print("Message received {s}", .{message});
-    //     const is_equal = std.mem.eql(u8, message, expected);
-    //
-    //     if (is_equal) {
-    //         try server.stream.writeAll("+PONG\r\n");
-    //     }
-    //
-    //     try stdout.print("{} says {s}\n", .{ client.address, message });
-    // }
+    // Uncomment this block to pass the first stage
+    const address = try net.Address.resolveIp("127.0.0.1", 6379);
 
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
-
-    const loopback = try net.Ip4Address.parse("127.0.0.1", 6379);
-    const localhost = net.Address{ .in = loopback };
-    var server = try localhost.listen(.{
-        .reuse_port = true,
+    var server = try address.listen(.{
+        .reuse_address = true,
     });
     defer server.deinit();
 
-    const addr = server.listen_address;
-    try stdout.print("Listening on {}, access this port to end the program\n", .{addr.getPort()});
+    while (true) {
+        var client = try server.accept();
 
-    var client = try server.accept();
-    defer client.stream.close();
+        try stdout.print("Connection received {} is sending data", .{client.address});
+        client.stream.close();
 
-    try stdout.print("Connection received! {} is sending data.\n", .{client.address});
+        // const expected = "PONG";
+        // const message = try client.stream.reader().readAllAlloc(allocator, expected.len);
+        // defer allocator.free(message);
 
-    const message = try client.stream.reader().readAllAlloc(allocator, 1024);
-    defer allocator.free(message);
-    try stdout.print("Message received {s}", .{message});
+        const message = "+PONG\r\n";
+        try server.stream.writeAll(message);
+
+        try stdout.print("{} says {s}\n", .{ client.address, message });
+    }
+
+    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    // defer _ = gpa.deinit();
+    // const allocator = gpa.allocator();
+    //
+    // const loopback = try net.Ip4Address.parse("127.0.0.1", 6379);
+    // const localhost = net.Address{ .in = loopback };
+    // var server = try localhost.listen(.{
+    //     .reuse_port = true,
+    // });
+    // defer server.deinit();
+    //
+    // const addr = server.listen_address;
+    // try stdout.print("Listening on {}, access this port to end the program\n", .{addr.getPort()});
+    //
+    // var client = try server.accept();
+    // defer client.stream.close();
+    //
+    // try stdout.print("Connection received! {} is sending data.\n", .{client.address});
+    //
+    // const message = try client.stream.reader().readAllAlloc(allocator, 1024);
+    // defer allocator.free(message);
+    // try stdout.print("Message received {s}", .{message});
     // const is_equal = std.mem.eql(u8, message, expected);
     //
     // if (is_equal) {
     //     try server.stream.writeAll("+PONG\r\n");
     // }
 
-    try stdout.print("{} says {s}\n", .{ client.address, message });
+    // try stdout.print("{} says {s}\n", .{ client.address, message });
 }
