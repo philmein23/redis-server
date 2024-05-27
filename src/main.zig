19409d156b625258e8def037aef24e0f8229bc9a
Phillip Nguyen <phillipnguyen@192.168.1.7>
2024-05-27 09:39:08 +0700

    test

--- src/main.zig
+++ src/main.zig
@@ -4,9 +4,9 @@ const net = std.net;
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
+    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    // defer _ = gpa.deinit();
+    // const allocator = gpa.allocator();
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     try stdout.print("Logs from your program will appear here!\n", .{});
 
