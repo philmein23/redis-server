8e346f26678866471bda53edd3806c42ad551d07
Phillip Nguyen <phillipnguyen@Phillips-MacBook-Air.local>
2024-05-30 01:37:12 +0700

    test

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,14 @@ const std = @import("std");
 // Uncomment this block to pass the first stage
 const net = std.net;
 
-fn write(server: *const net.Server) void {
+fn write() void {
+    const address = try net.Address.resolveIp("127.0.0.1", 6379);
+
+    var server = try address.listen(.{
+        .reuse_address = true,
+    });
+    defer server.deinit();
+
     var client_connection = try server.accept();
     var buffer: [1024]u8 = undefined;
 
@@ -31,14 +38,6 @@ pub fn main() !void {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     try stdout.print("Logs from your program will appear here!\n", .{});
 
-    // Uncomment this block to pass the first stage
-    const address = try net.Address.resolveIp("127.0.0.1", 6379);
-
-    var server = try address.listen(.{
-        .reuse_address = true,
-    });
-    defer server.deinit();
-
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
@@ -50,7 +49,7 @@ pub fn main() !void {
     const cpus = try std.Thread.getCpuCount();
 
     for (0..cpus) |_| {
-        try threads.append(try std.Thread.spawn(.{}, write, .{&server}));
+        try threads.append(try std.Thread.spawn(.{}, write, .{}));
     }
 
     for (threads.items) |thread| thread.join();
