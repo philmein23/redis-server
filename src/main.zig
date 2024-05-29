f7c5787aabe9ead6393ede794a4e79bcb4b67f56
Phillip Nguyen <phillipnguyen@192.168.1.4>
2024-05-29 13:30:43 +0700

    test

--- src/main.zig
+++ src/main.zig
@@ -44,7 +44,7 @@ pub fn main() !void {
 
     const allocator = gpa.allocator();
 
-    var threads = std.ArrayList(std.Thread).init(allocator);
+    var threads = std.ArrayList(*std.Thread).init(allocator);
     defer threads.deinit();
 
     const cpus = try std.Thread.getCpuCount();
