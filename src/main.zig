5bc5450e9f807dc59ce729b63713540ab2cf8986
Phillip Nguyen <phillipnguyen@Phillips-MacBook-Air.local>
2024-06-02 22:49:47 +0700

    test

--- src/main.zig
+++ src/main.zig
@@ -16,7 +16,9 @@ fn write(client_connection: net.Server.Connection) !void {
         const message = "+PONG\r\n";
         // std.mem.lastIndexOfScalar(comptime T: type, slice: []const T, value: T);
 
-        try stdout.print("Bytes read here: {}, buffer len: {}\n", .{ bytes_read, buffer.len });
+        for (0..buffer.len) |i| {
+            try stdout.print("Bytes read here: {}, buffer content: {}\n", .{ bytes_read, buffer[i] });
+        }
         _ = try client_connection.stream.writeAll(message);
 
         bytes_read = try reader.read(&buffer);
