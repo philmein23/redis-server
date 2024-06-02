8a5b0fd1cdb75029e99e969ac5852067dc4b6a83
Phillip Nguyen <phillipnguyen@Phillips-MacBook-Air.local>
2024-06-03 03:36:05 +0700

    test

--- src/main.zig
+++ src/main.zig
@@ -18,10 +18,9 @@ fn write(client_connection: net.Server.Connection, stdout: anytype) !void {
     // while (bytes_read > 0) {
     const message = "+PONG\r\n";
 
-    std.mem.lastIndexOfScalar([1024]u8, buffer, "echo");
+    // std.mem.lastIndexOfScalar([1024]u8, &buffer, "echo");
     const found_index = std.ascii.indexOfIgnoreCase(&buffer, "echo");
     try stdout.print("Found index: {}\n", .{found_index});
-
     _ = try client_connection.stream.writeAll(message);
 
     // bytes_read = try reader.read(&buffer);
