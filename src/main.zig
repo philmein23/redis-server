2b7d3b953c99585d471981fda0ad78435166349b
Phillip Nguyen <phillipnguyen@Phillips-MacBook-Air.local>
2024-06-10 00:18:13 +0700

    add parser

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,89 @@
 const std = @import("std");
 const net = std.net;
 
+const Loc = struct { start: usize, end: usize };
+const Tag = enum { echo, ping, arg };
+const Command = struct { loc: Loc, tag: Tag };
+const Arg = struct { loc: Loc, tag: Tag, content: []u8 };
+
+var Parser = struct {
+    buffer: []u8,
+    curr_index: usize,
+    pub fn parse(self: *Parser) !.{ Command, Arg } {
+        var command = Command{ .loc = Loc{ .start = undefined, .end = undefined }, .tag = undefined };
+        const arg = Arg{ .loc = Loc{ .start = undefined, .end = undefined }, .tag = undefined };
+        // bytes sent from client ex: "*2\r\n$4\r\nECHO\r\n$9\r\npineapple\r\n"
+        if (self.peek() == '*') {
+            self.next();
+        }
+
+        if (std.ascii.isDigit(self.peek())) {
+            self.next();
+        }
+
+        if (self.peek() == '\r') {
+            self.next();
+        }
+
+        if (self.peek() == '\n') {
+            self.next();
+        }
+
+        // parse command
+        if (std.ascii.isAlphabetic(self.peek())) {
+            self.next();
+            command.loc.start = self.curr_index;
+
+            while (true) {
+                if (std.ascii.isAlphabetic(self.peek())) {
+                    self.next();
+                    command.loc.end = self.curr_index;
+
+                    continue;
+                } else {
+                    break;
+                }
+            }
+
+            if (std.ascii.indexOfIgnoreCase(self.buffer[command.loc.start..command.loc.end], "echo")) |_| {
+                command.tag = Tag.echo;
+            }
+
+            if (std.ascii.indexOfIgnoreCase(self.buffer[command.loc.start..command.loc.end], "ping")) |_| {
+                command.tag = Tag.ping;
+            }
+        }
+
+        if (self.peek() == '\r') {
+            self.next();
+        }
+
+        if (self.peek() == '\n') {
+            self.next();
+        }
+
+        if (std.ascii.isDigit(self.peek())) {
+            self.next();
+        }
+
+        if (self.peek() == '\r') {
+            self.next();
+        }
+
+        if (self.peek() == '\n') {
+            self.next();
+        }
+
+        return .{ command, arg };
+    }
+    fn next(self: *Parser) void {
+        self.curr_index += 1;
+    }
+    fn peek(self: *Parser) u8 {
+        return self.buffer[self.curr_index + 1];
+    }
+};
+
 fn write(client_connection: net.Server.Connection, stdout: anytype) !void {
     defer client_connection.stream.close();
 
@@ -10,7 +93,6 @@ fn write(client_connection: net.Server.Connection, stdout: anytype) !void {
 
     try stdout.print("Connection received {} is sending data\n", .{client_connection.address});
 
-    // bytes sent from client ex: "*2\r\n$4\r\nECHO\r\n$9\r\npineapple\r\n"
     while (try reader.read(&buffer) > 0) {
         var command: []u8 = "";
         var byte_offset: usize = 8; // skip control sequence of bytes that dont need to be parsed
@@ -24,32 +106,17 @@ fn write(client_connection: net.Server.Connection, stdout: anytype) !void {
             }
             command = buffer[byte_offset..command_index_end];
 
-            // var to_lower_command: [5]u8 = undefined;
-            //
-            // for (0..command.len) |i| {
-            //     const c = std.ascii.toLower(command[i]);
-            //     to_lower_command[i] = c;
-            // }
-
-            // std.debug.print("Sliced command: {s}\n", .{&to_lower_command});
             std.debug.print("Sliced command: {s}\n", .{command});
 
             break;
         }
 
-        // if (std.mem.eql(u8, command, "ping")) {
-        //     _ = try client_connection.stream.writeAll("+PONG\r\n");
-        //     return;
-        // }
-        byte_offset = command_index_end + 1;
-
-        std.debug.print("Current byte offset: {}\n", .{byte_offset});
         if (std.ascii.indexOfIgnoreCase(command, "ping")) |_| {
             try client_connection.stream.writeAll("+PONG\r\n");
         } else {
+            // bytes sent from client ex: "*2\r\n$4\r\nECHO\r\n$9\r\npineapple\r\n"
             if (std.ascii.indexOfIgnoreCase(&buffer, command)) |fi| {
-                std.debug.print("Found index: {?}\n", .{fi});
-                std.debug.print("Byte encoding: {?}\n", .{buffer[fi]});
+                std.debug.print("Found starting index: {?}. Byte encoding: {?}\n", .{ fi, buffer[fi] });
 
                 byte_offset = fi + command.len - 1;
 
