b4343ca8a2233c3f9a7ddf104a4e7f8cb2e6dbe3
Phillip Nguyen <phillipnguyen@Phillips-MacBook-Air.local>
2024-06-17 04:29:34 +0700

    add set

--- src/main.zig
+++ src/main.zig
@@ -2,13 +2,22 @@ const std = @import("std");
 const net = std.net;
 
 const Loc = struct { start: usize, end: usize };
-const Tag = enum { echo, ping };
+const Tag = enum { echo, ping, set };
 const Command = struct { loc: Loc, tag: Tag, arg: Arg };
 const Arg = struct { loc: Loc, tag: Tag, content: []const u8 };
 
 const Parser = struct {
+    set_map: std.StringHashMap([]const u8),
     buffer: []const u8,
     curr_index: usize,
+
+    pub fn init(buffer: []const u8, alloc: std.mem.Allocator) Parser {
+        return Parser{ .set_map = std.StringHashMap([]const u8).init(alloc), .buffer = buffer, .curr_index = 0 };
+    }
+
+    pub fn deinit(self: *Parser) void {
+        self.set_map.deinit();
+    }
     pub fn parse(self: *Parser) !Command {
         const arg = Arg{ .loc = Loc{ .start = undefined, .end = undefined }, .tag = undefined, .content = undefined };
         var command = Command{ .loc = Loc{ .start = undefined, .end = undefined }, .tag = undefined, .arg = arg };
@@ -23,13 +32,7 @@ const Parser = struct {
             return error.ParsingErrorUnexpectedCharacter;
         }
 
-        if (self.peek() == '\r') {
-            self.next();
-        }
-
-        if (self.peek() == '\n') {
-            self.next();
-        }
+        try self.expect_return_new_line_bytes();
 
         if (self.peek() == '$') {
             self.next();
@@ -39,13 +42,7 @@ const Parser = struct {
             self.next();
         }
 
-        if (self.peek() == '\r') {
-            self.next();
-        }
-
-        if (self.peek() == '\n') {
-            self.next();
-        }
+        try self.expect_return_new_line_bytes();
 
         // parse command
         if (std.ascii.isAlphabetic(self.peek())) {
@@ -70,15 +67,13 @@ const Parser = struct {
             if (std.ascii.indexOfIgnoreCase(self.buffer[command.loc.start .. command.loc.end + 1], "ping")) |_| {
                 command.tag = Tag.ping;
             }
+
+            if (std.ascii.indexOfIgnoreCase(self.buffer[command.loc.start .. command.loc.end + 1], "set")) |_| {
+                command.tag = Tag.set;
+            }
         }
 
-        if (self.peek() == '\r') {
-            self.next();
-        }
-
-        if (self.peek() == '\n') {
-            self.next();
-        }
+        try self.expect_return_new_line_bytes();
 
         if (command.tag == Tag.ping) {
             return command;
@@ -92,13 +87,7 @@ const Parser = struct {
             self.next();
         }
 
-        if (self.peek() == '\r') {
-            self.next();
-        }
-
-        if (self.peek() == '\n') {
-            self.next();
-        }
+        try self.expect_return_new_line_bytes();
 
         if (std.ascii.isAlphanumeric(self.peek())) {
             self.next();
@@ -115,18 +104,51 @@ const Parser = struct {
                 }
             }
 
-            std.debug.print("Command ARG content {s}, source length {}\n", .{ self.buffer[command.arg.loc.start .. command.arg.loc.end + 1], self.buffer[command.arg.loc.start .. command.arg.loc.end + 1].len });
+            std.debug.print("Command ARG content {s}\n", .{self.buffer[command.arg.loc.start .. command.arg.loc.end + 1]});
 
             command.arg.content = self.buffer[command.arg.loc.start .. command.arg.loc.end + 1];
         }
 
-        if (self.peek() == '\r') {
-            self.next();
+        if (command.tag == Tag.ping) {
+            return command;
+        }
+        if (command.tag == Tag.set) {
+            try self.expect_return_new_line_bytes();
+
+            if (self.peek() == '$') {
+                self.next();
+            }
+
+            if (std.ascii.isDigit(self.peek())) {
+                self.next();
+            }
+
+            try self.expect_return_new_line_bytes();
+
+            if (std.ascii.isAlphanumeric(self.peek())) {
+                self.next();
+                var new_value = Arg{ .loc = undefined, .tag = .set, .content = undefined };
+                new_value.loc.start = self.curr_index;
+
+                while (true) {
+                    if (std.ascii.isAlphanumeric(self.peek())) {
+                        self.next();
+                        new_value.loc.end = self.curr_index;
+
+                        continue;
+                    } else {
+                        break;
+                    }
+                }
+
+                new_value.content = self.buffer[new_value.loc.start .. new_value.loc.end + 1];
+
+                std.debug.print("SET mapping key: {s}, value: {s}\n", .{ command.arg.content, new_value.content });
+                try self.set_map.put(command.arg.content, new_value.content);
+            }
         }
 
-        if (self.peek() == '\n') {
-            self.next();
-        }
+        try self.expect_return_new_line_bytes();
 
         return command;
     }
@@ -136,22 +158,61 @@ const Parser = struct {
     fn peek(self: *Parser) u8 {
         return self.buffer[self.curr_index + 1];
     }
+
+    fn expect_return_new_line_bytes(self: *Parser) !void {
+        if (self.peek() == '\r') {
+            self.next();
+        } else {
+            return error.ExpectedCarriageReturnByte;
+        }
+
+        if (self.peek() == '\n') {
+            self.next();
+        } else {
+            return error.ExpectedNewLineByte;
+        }
+    }
 };
+
+test "test SET command" {
+    const bytes = "*3\r\n$3\r\nSET\r\n$5\r\napple\r\n$4\r\npear\r\n";
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const alloc = gpa.allocator();
+    var parser = Parser.init(bytes, alloc);
+    const command = try parser.parse();
+
+    std.debug.print("Command ARG content: {s}\n", .{command.arg.content});
+    try std.testing.expectEqual(Tag.set, command.tag);
+
+    // parser.deinit();
+}
+
+test "test GET command" {}
 
 test "test parse PING command" {
     const bytes = "*1\r\n$4\r\nping\r\n";
-    var parser = Parser{ .buffer = bytes, .curr_index = 0 };
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
 
+    const alloc = gpa.allocator();
+    var parser = Parser.init(bytes, alloc);
     const command = try parser.parse();
 
     std.debug.print("Command ARG content: {s}\n", .{command.arg.content});
     try std.testing.expectEqual(Tag.ping, command.tag);
+
+    parser.deinit();
 }
 
 test "test parse ECHO command" {
     const bytes = "*2\r\n$4\r\nECHO\r\n$9\r\npineapple\r\n";
-    var parser = Parser{ .buffer = bytes, .curr_index = 0 };
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
 
+    const alloc = gpa.allocator();
+    var parser = Parser.init(bytes, alloc);
     const command = try parser.parse();
 
     std.debug.print("Command ARG content: {s}\n", .{command.arg.content});
@@ -160,6 +221,8 @@ test "test parse ECHO command" {
     std.debug.print("Command ARG expected ptr: {*}, actual ptr: {*}\n", .{ exp.ptr, command.arg.content.ptr });
     std.debug.print("Command ARG expected slice: {s}, actual slice: {s}\n", .{ exp, command.arg.content });
     try std.testing.expectEqualSlices(u8, exp, command.arg.content);
+
+    parser.deinit();
 }
 
 fn handle_echo(client_connection: net.Server.Connection, arg: Arg) !void {
@@ -200,64 +263,6 @@ fn handle_connection(client_connection: net.Server.Connection, stdout: anytype) 
             Tag.echo => try handle_echo(client_connection, command.arg),
             Tag.ping => try handle_ping(client_connection),
         }
-
-        // var command: []u8 = "";
-        // var byte_offset: usize = 8; // skip control sequence of bytes that dont need to be parsed
-        // var command_index_end = byte_offset;
-        // while (true) {
-        //     if (std.ascii.isAlphabetic(buffer[command_index_end])) {
-        //         std.debug.print("Command char: {?}\n", .{buffer[command_index_end]});
-        //         command_index_end += 1;
-        //
-        //         continue;
-        //     }
-        //     command = buffer[byte_offset..command_index_end];
-        //
-        //     std.debug.print("Sliced command: {s}\n", .{command});
-        //
-        //     break;
-        // }
-        //
-        // if (std.ascii.indexOfIgnoreCase(command, "ping")) |_| {
-        //     try client_connection.stream.writeAll("+PONG\r\n");
-        // } else {
-        //     // bytes sent from client ex: "*2\r\n$4\r\nECHO\r\n$9\r\npineapple\r\n"
-        //     if (std.ascii.indexOfIgnoreCase(&buffer, command)) |fi| {
-        //         std.debug.print("Found starting index: {?}. Byte encoding: {?}\n", .{ fi, buffer[fi] });
-        //
-        //         byte_offset = fi + command.len - 1;
-        //
-        //         byte_offset += 7; // skip non-alphabetic bytes
-        //         std.debug.print("Starting character: {?}\n", .{buffer[byte_offset]});
-        //
-        //         var end_index = byte_offset;
-        //         var string: []u8 = "";
-        //         while (true) {
-        //             if (std.ascii.isAlphabetic(buffer[end_index])) {
-        //                 std.debug.print("Char to be echoed: {?}\n", .{buffer[end_index]});
-        //                 end_index += 1;
-        //
-        //                 continue;
-        //             }
-        //             string = buffer[byte_offset..end_index];
-        //
-        //             break;
-        //         }
-        //
-        //         const terminator = "\r\n";
-        //         const length = string.len;
-        //
-        //         var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-        //         defer _ = gpa.deinit();
-        //
-        //         const allocator = gpa.allocator();
-        //
-        //         const resp = try std.fmt.allocPrint(allocator, "${d}{s}{s}{s}", .{ length, terminator, string, terminator });
-        //         defer allocator.free(resp);
-        //
-        //         _ = try client_connection.stream.writeAll(resp);
-        //     }
-        // }
     }
 }
 
