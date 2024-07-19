83fffbf3495def4bb66242b6ebe00054f6ffac98
Phillip Nguyen <phillipnguyen@Phillips-MacBook-Air.local>
2024-07-19 13:28:11 +0700

    wip

--- spawn_redis_server.sh
+++ spawn_redis_server.sh
@@ -1,24 +1,11 @@
-s#!/bin/sh
-#
-# Use this script to run your program LOCALLY.
-#
-# Note: Changing this script WILL NOT affect how CodeCrafters runs your program.
-#
-# Learn more: https://codecrafters.io/program-interface
 
-set -e # Exit early if any commands fail
-
-# Copied from .codecrafters/compile.sh
+#!/bin/sh
 #
-# - Edit this to change how your program compiles locally
-# - Edit .codecrafters/compile.sh to change how your program compiles remotely
-(
-  cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
-  zig build
-)
-
-# Copied from .codecrafters/run.sh
+# DON'T EDIT THIS!
 #
-# - Edit this to change how your program runs locally
-# - Edit .codecrafters/run.sh to change how your program runs remotely
-exec zig-out/bin/zig "$@"
+# CodeCrafters uses this file to test your code. Don't make any changes here!
+#
+# DON'T EDIT THIS!
+set -e
+
+exec zig buikkld run -- "$@"
