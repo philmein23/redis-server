f7df60089b8aa257cf3ef5b57ab1a08ea9cc1929
Phillip Nguyen <phillipnguyen@Phillips-MacBook-Air.local>
2024-07-22 13:07:01 +0700

    wip

--- src/main.zig
+++ src/main.zig
@@ -332,7 +332,7 @@ fn handle_info(client_connection: net.Server.Connection, is_replica: bool) !void
         }
     }
 
-    const resp = try std.fmt.allocPrint(allocator, "${s}", .{random_int_buffer});
+    const resp = try std.fmt.allocPrint(allocator, "${s}", .{&random_int_buffer});
     // const resp = try std.fmt.allocPrint(allocator, "${d}{s}{s}{s}", .{ val.len, terminator, val, terminator });
     defer allocator.free(resp);
 
