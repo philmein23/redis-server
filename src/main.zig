0f9bc24aebf68af5a26627a609c44522a1537476
Phillip Nguyen <phillipnguyen@Phillips-MacBook-Air.local>
2024-07-28 02:21:03 -0700

    wip

--- src/main.zig
+++ src/main.zig
@@ -492,7 +492,7 @@ pub fn main() !void {
         var buffer: [1024:0]u8 = undefined;
         const bytes_read = replica_stream.?.read(&buffer);
 
-        std.debug.print("REPLICA STREAM BYTES READ {any}, {s}", .{ bytes_read, buffer });
+        std.debug.print("REPLICA STREAM BYTES READ {any}, {s}", .{ bytes_read, &buffer });
     }
 
     const allocator = gpa.allocator();
