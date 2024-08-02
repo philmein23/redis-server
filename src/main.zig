15f85f3f88512edeb6144dbdb8000760b93081ba
Phillip Nguyen <phillipnguyen@Phillips-MacBook-Air.local>
2024-08-01 20:44:03 -0700

    wip

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,7 @@ const time = std.time;
 const rand = std.crypto.random;
 const Loc = struct { start: usize, end: usize };
 const Tag = enum { echo, ping, set, get, info, replconf, psync };
-const Command = struct { loc: Loc, tag: Tag, args: []Arg, opt: ?Arg = null };
+const Command = struct { loc: Loc, tag: Tag, args: [2]Arg, opt: ?Arg = null };
 const Arg = struct { loc: Loc, tag: Tag, content: []const u8 };
 
 const RedisStore = struct {
@@ -506,7 +506,7 @@ fn handle_connection(client_connection: net.Server.Connection, stdout: anytype, 
             Tag.get => try handle_get(client_connection, &store, command.args[0]),
             Tag.info => try handle_info(client_connection, is_replica, &master_replication_id),
             Tag.replconf => try handle_replconf(client_connection),
-            Tag.psync => try handle_psync(allocator, client_connection, &master_replication_id, command.args),
+            Tag.psync => try handle_psync(allocator, client_connection, &master_replication_id, &command.args),
         }
     }
 }
