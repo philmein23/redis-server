d62e2a4990dc33639c44c36c7648726b3d64bc30
Phillip Nguyen <phillipnguyen@Phillips-MacBook-Air.local>
2024-08-11 20:42:39 -0700

    wip

--- src/main.zig
+++ src/main.zig
@@ -731,18 +731,18 @@ pub fn main() !void {
         _ = try replica_stream.read(&buffer); // reads empty RDB file from master
 
         std.debug.print("Replica synchronized with master...\n", .{});
-        const thread = try std.Thread.spawn(
-            .{},
-            handle_connection,
-            .{
-                replica_stream,
-                stdout,
-                allocator,
-                &state,
-                &store,
-            },
-        );
-        thread.detach();
+        // const thread = try std.Thread.spawn(
+        //     .{},
+        //     handle_connection,
+        //     .{
+        //         replica_stream,
+        //         stdout,
+        //         allocator,
+        //         &state,
+        //         &store,
+        //     },
+        // );
+        // thread.detach();
     }
 
     while (true) {
